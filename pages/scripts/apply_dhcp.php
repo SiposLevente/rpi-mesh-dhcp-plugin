4087e2c25c4a4c05b0bc3754463ceade9fc624c0
Sipos_Levente_Szabolcs <siposleventeszabolcs@stud.uni-obuda.hu>
2022-10-10 21:08:41 +0200

    optional router and nameservers

--- pages/scripts/apply_dhcp.php
+++ pages/scripts/apply_dhcp.php
@@ -6,8 +6,21 @@ $range1 = $_POST["range-1"];
 $range2 = $_POST["range-2"];
 $router = $_POST["router"];
 $dns1 = $_POST["dns1"];
+$dns2 = $_POST["dns2"];
 
-shell_exec("sudo /etc/node-scripts/apply_dnsmasq.sh $interfaces $lease_time $netmask $range1 $range2 $router $dns1");
+if ($router == "") {
+    $router = "null"
+}
+
+if ($dns1 == "") {
+    $dns1 = "null"
+}
+
+if ($dns2 == "") {
+    $dns2 = "null"
+}
+
+shell_exec("sudo /etc/node-scripts/apply_dnsmasq.sh $interfaces $lease_time $netmask $range1 $range2 $router $dns1 dns1");
 
 header('Location: ../settings.html');
 ?>
--- setup_scripts/node_scripts/apply_dnsmasq.sh
+++ setup_scripts/node_scripts/apply_dnsmasq.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 echo "interface=$1" > /etc/dnsmasq.conf
 echo "dhcp-range=$4,$5,$3,$2s" >> /etc/dnsmasq.conf
+if [[ $6 != "null" ]];then
 echo "dhcp-option=option:router,$6" >> /etc/dnsmasq.conf
+fi
+if [[ $7 != "null" ]];then
 echo "server=$7" >> /etc/dnsmasq.conf
+fi
+if [[ $8 != "null" ]];then
+echo "server=$8" >> /etc/dnsmasq.conf
+fi
