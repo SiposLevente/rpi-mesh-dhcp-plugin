642ead4d8c27afa1886bf8a5bb7137c3f47f981f
Sipos_Levente_Szabolcs <siposleventeszabolcs@stud.uni-obuda.hu>
2022-10-10 20:42:46 +0200

    added disabling and enabling to dnsmasq config

--- setup_scripts/node_scripts/toggle_dnsmasq.sh
+++ setup_scripts/node_scripts/toggle_dnsmasq.sh
@@ -5,6 +5,8 @@ status="`systemctl is-active dnsmasq.service`"
 if [[ $status == 'active' ]];
 then
 	systemctl stop dnsmasq.service
+	systemctl disable dnsmasq.service
 else
 	systemctl start dnsmasq.service
+	systemctl enable dnsmasq.service
 fi
