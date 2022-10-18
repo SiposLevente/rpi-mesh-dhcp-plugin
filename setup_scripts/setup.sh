ef59312a33e87f5b45d40c819583339120d1ef98
Sipos_Levente_Szabolcs <siposleventeszabolcs@stud.uni-obuda.hu>
2022-10-18 16:10:33 +0200

    made script installable locally and by script manager

--- setup_scripts/setup.sh
+++ setup_scripts/setup.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-
 if [[ `whoami` != "root" ]];then
     echo "Run this script with root privilages!"
     exit 1
 fi
 
+if [[ $1 == "local" ]];then
+  node_script_location="node_scripts/*"
+else
+  node_script_location="/var/www/html/plugins/rpi-mesh-dhcp-plugin/setup_scripts/node_scripts/*"
+fi
 
 echo "Installing needed packages..."
 apt install -y dnsmasq
@@ -13,9 +17,9 @@ systemctl disable dnsmasq
 
 echo "Copying scripts /etc/node-scripts/"
 mkdir -p /etc/node-scripts/
-cp -r ./node_scripts/* /etc/node-scripts/
+cp -r node_script_location /etc/node-scripts/
 
-SCRIPTS="node_scripts/*"
+SCRIPTS=$node_script_location
 for file in $SCRIPTS
 do
   is_in_file=`grep "www-data ALL = NOPASSWD: /etc/node-scripts/${file##*/}" /etc/sudoers`
